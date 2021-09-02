80ef167e2c94ed9361399505fc3475e09fe7745c
Andrés Bajac <andresbajac@gmail.com>
2021-09-01 23:01:23 -0300

    Agrego carpeta components con Navbar.js

---
+++ src/Components/NavBar.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+//import './styles.css'
+
+const NavBar = () => {
+    return (
+            <nav>
+                <p>Home</p>
+                <p>Productos</p>
+                <p>Promos</p>
+                <p>Contacto</p>
+            </nav>
+    );
+}
+
+export default NavBar
